4fb60378cc71b88bacee8c41880c31d19d8a5057
Amit Yadav <145849077+Amityadav9315@users.noreply.github.com>
2024-10-02 01:58:05 +0530

    PalindromeCheck

--- src/Palindromecheck.java
+++ src/Palindromecheck.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Palindromecheck {
+import java.util.Scanner;
+
+public class Palindromecheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int n=str.length();
+        int count=0,count1=0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            //System.out.print(ch);
+            if(str.charAt(i)==str.charAt(n-1-i)){
+                count++;
+
+            }
+            else{
+                count1++;
+            }
+        }
+        if(count1>1){
+            System.out.println("Not palindrome");
+        }
+        else{
+            System.out.println("Palindrome");
+        }
+
+    }
 }
